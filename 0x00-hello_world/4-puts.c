2dca84742dc65c377d23a7c291b8e84bbd7bd09c
sophiathegem <sophiathegem.com>
2023-02-16 16:16:46 -0800

    programming is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a statement using puts
+ *
+ * Return: Always 0( success)
+ */
+int main(void) /* Return :0(sucesss) ,don't print result */
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
