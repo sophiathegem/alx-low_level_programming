1660ed9bea877a6199cb7206175355e1a7eed1c8
sophiathegem <sophiathegem.com>
2023-03-06 19:26:47 -0800

    pointers and arrays

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
  * set_string - sets the value of a pointer to a char
- * @s: source adress.
- * @to: target adress.
- * Return: no return.
+ * @s: pointer to change
+ * @to: string to change pointer to
+ *
+ * Return: void
  */
 void set_string(char **s, char *to)
 {
